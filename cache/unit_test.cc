0c27e3c4dc9a2dd03846c563fe127f579268fa69
ka <todo1@rotarydef.net>
2023-10-27 09:25:58 -0700

    class

--- cache/unit_test.cc
+++ cache/unit_test.cc
@@ -1,48 +1,7 @@
 #ifdef TMAIN_KAUTIL_CACHE_INTERFACE
 
-
-
-#include "kautil/range/exists/exists.hpp"
-#include "kautil/range/merge/merge.hpp"
-#include "kautil/range/gap/gap.hpp"
-
-template<typename pref_t>
-struct cache 
-        : private kautil::range::merge<pref_t>
-        , private kautil::range::exists<pref_t>
-        , private kautil::range::gap<pref_t>
-        {
-    using value_type = typename pref_t::value_type;
-    using offset_type = typename pref_t::offset_type;
-    
-    cache(pref_t * pref) : pref(pref)
-        ,kautil::range::merge<pref_t>(pref) 
-        ,kautil::range::exists<pref_t>(pref) 
-        ,kautil::range::gap<pref_t>(pref)
-    {
-        
-    }
-    ~cache(){}
-    
-    using kautil::range::merge<pref_t>::set_diff;
-    bool exists(value_type from, value_type to){ return kautil::range::exists<pref_t>::exec(from,to); }
-    int merge(value_type from, value_type to){ return kautil::range::merge<pref_t>::exec(from,to); }
-    kautil::range::gap<pref_t> gap(value_type from, value_type to){
-        kautil::range::gap<pref_t>::initialize(from,to);
-        return *this;
-    }
-    
-    
-    pref_t * pref= 0;
-};
-
-
-
 #include "cache.hpp"
 #include "stdio.h"
-
-
-
 #include <stdint.h>
 #include <fcntl.h>
 #include <stdlib.h>
@@ -102,21 +61,17 @@ using file_syscall_16b_f_pref= file_syscall_premitive<double>;
 
 
 int main(){
-    
-    
     // todo : kautil::range::merge<pref_t>::set_buffer may be curious.
+    
+    
     using value_type = uint64_t;
     using offset_type = long;
     auto f_ranges = fopen("tmain_kautil_range_exsits_interface.cache","w+b");
-    auto pref = file_syscall_16b_pref{.fd=fileno(f_ranges),.buffer_size=512};
-    
+    auto pref = file_syscall_16b_pref{.fd=fileno(f_ranges)};
     auto from = value_type(0),to = value_type(0),diff=value_type(0);
     diff=1;
     from = 10;to = 15;
-    auto c = cache{&pref};
-    c.set_diff(diff);
-    
-    
+    auto c = cache{&pref,diff,512};
     auto lmb_test = [](auto * c,auto from,auto to){
         if(!c->exists(from,to)){
             printf("not exists\n");
@@ -128,7 +83,7 @@ int main(){
     
     lmb_test(&c,from,to);
     lmb_test(&c,from,to);
-    for(auto const& elem : c.gap(0,20))printf("l,r(%lld,%lld)\n",elem.l,elem.r);
+    for(auto const& elem : c.gap(0,20))printf("l,r(%lld,%lld)\n", elem.l, elem.r);
     return 0;
 }
 
