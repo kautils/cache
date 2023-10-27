6e09305128e2c47a5ef1ae389bcafb5f27bba772
ka <todo1@rotarydef.net>
2023-10-27 08:59:49 -0700

    class > exists

--- cache/unit_test.cc
+++ cache/unit_test.cc
@@ -11,8 +11,8 @@ struct cache
         , private kautil::range::exists<pref_t>
         , private kautil::range::gap<pref_t>
         {
-    using value_type = pref_t::value_type;
-    using offset_type = pref_t::offset_type;
+    using value_type = typename pref_t::value_type;
+    using offset_type = typename pref_t::offset_type;
     
     cache(pref_t * pref) : pref(pref)
         ,kautil::range::merge<pref_t>(pref) 
@@ -39,15 +39,84 @@ struct cache
 
 
 
+#include <stdint.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
+
+
+template<typename premitive>
+struct file_syscall_premitive{
+    using value_type = premitive;
+    using offset_type = long;
+
+    int fd=-1;
+    char * buffer = 0;
+    offset_type buffer_size = 0;
+    
+    ~file_syscall_premitive(){ free(buffer); }
+    offset_type block_size(){ return sizeof(value_type); }
+    offset_type size(){ return lseek(fd,0,SEEK_END)- lseek(fd,0,SEEK_SET); }
+    
+    void read_value(offset_type const& offset, value_type ** value){
+        lseek(fd,offset,SEEK_SET);
+        ::read(fd,*value,sizeof(value_type));
+    }
+    
+    bool write(offset_type const& offset, void ** data, offset_type size){
+        lseek(fd,offset,SEEK_SET);
+        return size==::write(fd,*data,size);
+    }
+    
+    // api may make a confusion but correct. this is because to avoid copy of value(object such as bigint) for future.
+    bool read(offset_type const& from, void ** data, offset_type size){
+        lseek(fd,from,SEEK_SET);
+        return size==::read(fd,*data,size);
+    }
+    
+    bool extend(offset_type extend_size){ 
+        return !ftruncate(fd,size()+extend_size);   
+    }
+    int shift(offset_type dst,offset_type src,offset_type size){
+        if(buffer_size < size){
+            if(buffer)free(buffer);
+            buffer = (char*) malloc(buffer_size = size);
+        }
+        lseek(fd,src,SEEK_SET);
+        auto read_size = ::read(fd,buffer,size);
+        lseek(fd,dst,SEEK_SET);
+        ::write(fd,buffer,read_size);
+        return 0;
+    }
+    
+    int flush_buffer(){ return 0; }
+};
+
+using file_syscall_16b_pref= file_syscall_premitive<uint64_t>;
+using file_syscall_16b_f_pref= file_syscall_premitive<double>;
+
+
+
+//#include <vector>
 
 int main(){
     
     
     
+    using value_type = uint64_t;
+    using offset_type = long;
+    auto f_ranges = fopen("tmain_kautil_range_exsits_interface.cache","w+b");
+    auto pref = file_syscall_16b_pref{.fd=fileno(f_ranges)};
+    
+    auto from = value_type(0),to = value_type(0);
+    from = 10;to = 15;
+    auto c = cache{&pref};
+    auto exists = c.exists(from,to);
+    printf("%s\n",exists ? "exists":"not exists");
     
     
-    printf("++++\n");
+    
+    
     return 0;
 }
 
